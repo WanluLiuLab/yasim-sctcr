8296cb5a76cbffdd1ca7ce8eaae1253604aa34b2
yuzj <theafamily@126.com>
2024-03-27 17:28:44 +0800

    Small bugs fixed

--- explore/huardb_gex/sim.R
+++ explore/huardb_gex/sim.R
@@ -1,21 +1,21 @@
 library("SPARSim")
 library("Seurat")
 
-df <-CreateSeuratObject(
-    Seurat::Read10X("C12_PBMC/filtered_feature_bc_matrix")
-)
-df <- subset(df, nFeature_RNA > 200 & nCount_RNA > 500)
 
-cm  <- as.matrix(df[["RNA"]]$counts)
-ncm <- SPARSim::scran_normalization(cm)
-
-params <- SPARSim::SPARSim_estimate_parameter_from_data(
-    cm,
-    ncm,
-    list(a=colnames(cm))
-)
-names(params[[1]]$lib_size) <- colnames(cm)
-
-sim_result <- SPARSim_simulation(params)
-
-T_10X_like_dataset <- SPARSim_simulation(T_10X_param_preset)
+for (data_name in c("C12_PBMC", "CPInc_NC1", "nc_CT1", "U4_PBMC")) {
+    df <- CreateSeuratObject(
+        Seurat::Read10X(sprintf("%s/filtered_feature_bc_matrix", data_name))
+    )
+    df <- subset(df, nFeature_RNA > 200 & nCount_RNA > 500)
+    cm <- as.matrix(df[["RNA"]]$counts)
+    ncm <- SPARSim::scran_normalization(cm)
+    params <- SPARSim::SPARSim_estimate_parameter_from_data(
+        cm,
+        ncm,
+        list(a = colnames(cm))
+    )
+    names(params[[1]]$lib_size) <- colnames(cm)
+    sim_result <- SPARSim_simulation(params)
+    write.csv(cm, sprintf("%s.cnt_mtx.csv", data_name))
+    write.csv(sim_result$count_matrix, sprintf("%s.sim_mtx.csv", data_name))
+}
